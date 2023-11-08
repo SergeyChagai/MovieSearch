f70a8efaf348989ee217e182661385817ffdcbad
Chagaj Sergey <chagaj42@gmail.com>
2023-11-08 14:47:53 +0400

    Add Models and CRUD layer

---
+++ MovieSearch/MovieSearch/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieSearch.Models
+{
+    [Table("Genres")]
+    public class Genre
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MovieSearch/MovieSearch/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieSearch.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Genre> genres { get; set; }
+        public List<Actor> actors {get;set;}
+    }
+}
